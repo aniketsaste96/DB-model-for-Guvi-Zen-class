0846afbe9de2698315c6bdefa465a0e9088e192c
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2022-01-22 17:45:20 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,38 @@
+//TASKS
 
+> db.tasks.find({})
+{ "_id" : ObjectId("61ebf42ee28138cb99847106"), "id" : "1", "name" : "CRUD_APP" }
+{ "_id" : ObjectId("61ebf42ee28138cb99847107"), "id" : "2", "name" : "REDUX" }
+{ "_id" : ObjectId("61ebf42ee28138cb99847108"), "id" : "3", "name" : "FORMIK" }
+{ "_id" : ObjectId("61ebf42ee28138cb99847109"), "id" : "4", "name" : "DB_DESIGN" }
+{ "_id" : ObjectId("61ebf42ee28138cb9984710a"), "id" : "5", "name" : "DASHBOARD" }
+>
+
+//MENTORS
+> db.mentors.find({})
+{ "_id" : ObjectId("61ebf0cfe28138cb998470fc"), "id" : "1", "name" : "RAGAV KUMAR V", "student_assigned" : "25" }
+{ "_id" : ObjectId("61ebf0cfe28138cb998470fd"), "id" : "2", "name" : "prasad", "student_assigned" : "30" }
+{ "_id" : ObjectId("61ebf0cfe28138cb998470fe"), "id" : "3", "name" : "lavish", "student_assigned" : "10" }
+{ "_id" : ObjectId("61ebf0cfe28138cb998470ff"), "id" : "4", "name" : "mehul", "student_assigned" : "8" }
+{ "_id" : ObjectId("61ebf0cfe28138cb99847100"), "id" : "5", "name" : "Mariappan", "student_assigned" : "20" }
+>
+
+
+//STUDENTS
+
+> db.students.find({})
+{ "_id" : ObjectId("61ebefcae28138cb998470f2"), "id" : "1", "name" : "keval", "age" : "25", "gender" : "M" }
+{ "_id" : ObjectId("61ebefcae28138cb998470f3"), "id" : "2", "name" : "NEHA", "age" : "29", "gender" : "F" }
+{ "_id" : ObjectId("61ebefcae28138cb998470f4"), "id" : "3", "name" : "ASHFAQ", "age" : "32", "gender" : "M" }
+{ "_id" : ObjectId("61ebefcae28138cb998470f5"), "id" : "4", "name" : "SANJAY", "age" : "23", "gender" : "M" }
+{ "_id" : ObjectId("61ebefcae28138cb998470f6"), "id" : "5", "name" : "NIKITA", "age" : "27", "gender" : "F" }
+>
+
+//BATCH
+> db.batch.find({})
+{ "_id" : ObjectId("61ebf04de28138cb998470f7"), "id" : "1", "name" : "B26" }
+{ "_id" : ObjectId("61ebf04de28138cb998470f8"), "id" : "2", "name" : "B27" }
+{ "_id" : ObjectId("61ebf04de28138cb998470f9"), "id" : "3", "name" : "B28" }
+{ "_id" : ObjectId("61ebf04de28138cb998470fa"), "id" : "4", "name" : "B29" }
+{ "_id" : ObjectId("61ebf04de28138cb998470fb"), "id" : "5", "name" : "B29WE" }
+>
